c46614569874e319b5cc15cdae55a60f678ef22f
Alexis <avannier@fluksaqua.com>
2019-07-04 23:21:14 +0200

    Add zlib to build and shared object ON

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ mkdir -p $builddir/out/include
 echo "Compiling cpp-driver..."
 cd $builddir/cpp-driver
 cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$builddir -DCASS_BUILD_STATIC=ON \
-  -DCASS_BUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCASS_USE_ZLIB=ON \
+  -DCASS_BUILD_SHARED=ON -DCMAKE_BUILD_TYPE=RELEASE -DCASS_USE_ZLIB=ON \
   -DCMAKE_INSTALL_LIBDIR:PATH=lib $basedir/php-driver/lib/cpp-driver/
 make
 
